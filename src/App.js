4bb9fa15d2056faeaa3b668cabaf1ebd7f5987f1
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-02-24 18:47:22 +0100

    default

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./lib/font-awesome/css/all.min.css";
+//
+//
+//
+import Home from "./components/Home";
+import { Navigation } from "./components/Navigation";
+
+//
+//
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <div className="page">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route path="/watched" exact component={Watched} /> */}
+          <Route path="/*" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
