c2e681f7b3fceb28055b559a7620f87069ad46cf
nadiamariduena <nadia.mm.mariduena@gmail.com>
2022-02-25 21:08:30 +0100

    styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import { mobile, mobileM, tablet } from "../responsive";
+
+const Header = styled.header`
+  width: 100vw;
+  min-height: 3vh;
+  padding: 1vh 0;
+  background: #1ba1aa;
+`;
+const Nav = styled.nav`
+  width: 100%;
+  height: 100%;
+
+  background: #b7dad5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: calc(5px + 1.8vmin);
+  text-transform: uppercase;
+  font-weight: 600;
+`;
 
 export const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <Header>
+      <Nav>hello</Nav>
+    </Header>
+  );
 };
